01786a8da1edb6b3f821cdd53c7481fee3c76b0c
jaemin2505 <kjm25052@naver.com>
2026-04-03 10:12:16 +0900

    최신화
    최신화

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import App from './App'
 import './styles/base.css'
 import './styles/layout.css'
 import './styles/public.css'
+import './styles/landing.css'
 import './styles/console.css'
 import './styles/responsive.css'
 
