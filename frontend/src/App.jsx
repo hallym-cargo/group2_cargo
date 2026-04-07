e083f451ffefe7aa5a1b84a15f13075b63ad53f7
hyojeong21 <seohyojeong123@gmail.com>
2026-04-07 16:30:15 +0900

    회원가입 페이지 완성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,7 @@ import { useLogisticsController } from "./hooks/useLogisticsController";
 import TransportStatus from "./pages/TransportStatus";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import ShipperSignupPage from "./pages/ShipperSignupPage";
-import DriverSignupPage from "./pages/DriverSignupPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 
 export default function App() {
   const controller = useLogisticsController();
@@ -43,6 +42,8 @@ export default function App() {
     page = <ShipperSignupPage controller={controller} />;
   } else if (controller.routePage === "signup-driver") {
     page = <DriverSignupPage controller={controller} />;
+  } else if (controller.routePage === "forgot-password") {
+    page = <ForgotPasswordPage controller={controller} />;
   } else if (controller.dashboardTab === "home") {
     page = <PublicHomePage controller={controller} />;
   } else if (controller.isAdmin) {
